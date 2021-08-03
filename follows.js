9148c1f2c1b76411c54fc8276f6ca8f4df4ab609
alikaakati <kaakatiali@gmail.com>
2021-08-03 23:52:41 +0300

    added toggleFollowUser

--- follows.js
+++ follows.js
@@ -23,6 +23,64 @@ class Follow {
 class Follows {
   static TAG = 'follows';
   
+  static toggleFollowUser = (fUserId , callback) =>{
+    if(Follows.validData(fUserId)){
+      buildfire.auth.getCurrentUser((err , user) => {        
+        if(err || !user) return callback("You must be logged in to follow a user" , null);
+        else {
+          buildfire.appData.search({filter :{"$json.userId" : user._id}} , Follows.TAG , async(err , resp) =>{
+            if(err) return callback(err , "first return callback");
+            else if(!(Array.isArray(resp))) return callback(resp , null);
+            else if(resp.length == 0){
+              let saveObj = {
+                userId : user._id , 
+                followedUsers : [fUserId] ,
+                followedPlugins : [],
+                createdBy : `${authManager.currentUser.email} - ${authManager.currentUser.username}` , 
+                createdOn : new Date()
+              } 
+              
+              return buildfire.appData.insert(saveObj , Follows.TAG, (error, record) => {
+                if (error) return callback(error , null);
+                return callback(null, record);
+              });
+            }
+            else {
+              let {data , id} = resp[0];
+              let followedUsers = data.followedUsers;
+              if(Follows.isValidUser(fUserId)){
+                let index = followedUsers.findIndex(e => e == fUserId);
+                if(index >= 0){
+                  let newFollowedUsers = [...followedUsers];
+                  newFollowedUsers.splice(index , 1);
+                  let update = {...data , followedUsers : newFollowedUsers};
+                  return await buildfire.appData.update(id , update , Follows.TAG, (err , resp) =>{
+                    if(err) return callback(err , null)
+                    else return callback(null , resp)
+                  });
+                }
+                else{
+                  let update = {...data , followedUsers : [...followedUsers , fUserId]};
+                  return await buildfire.appData.update(id , update , Follows.TAG, (err , resp) =>{
+                    if(err) return callback(err , null)
+                    else return callback(null , resp)
+                  })
+                }
+              }
+              else {
+                return callback("Invalid user ID" , null);
+              }
+            }
+          })
+        }
+      })
+    }
+    else{
+      return callback("Malformatted data" , null);
+    }
+  }
+
+
   static followUser = (fUserId , callback) =>{
     
     if(Follows.validData(fUserId)){
