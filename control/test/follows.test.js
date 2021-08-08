78aebe28d0eed997549480252e92559efb050e9f
Nadia Khouri <>
2021-08-08 22:23:01 +0300

    new test scenario for user func

--- control/test/follows.test.js
+++ control/test/follows.test.js
@@ -11,14 +11,14 @@ let followsTest = {
         }
     },
     testFollowUser : function(){
+        //check follow functionality
         if(typeof(Follows.followUser)=="undefined"){
             console.error("follow user exist failed");
         }
         else{
-            //Follows.clearAppData();
-            // delete if you want to keep the follows data
             console.log("follow user exist passed");
             Follows.followUser("" ,(err,resp) =>{
+                //check follow functionality for empty ID
                 if(err){
                     console.error("Checking follow user with an empty ID:");
                     console.error(err);
@@ -29,6 +29,7 @@ let followsTest = {
                 }
             });
             Follows.followUser("60f49004784379051298ea37" ,(err,resp) =>{
+                //Check follow valid id
                 if(err){
                     console.error("Checking follow user with a vaild ID:");
                     console.error(err);
@@ -39,6 +40,7 @@ let followsTest = {
                 }
             });
             Follows.followUser("60f49004784379051298ea37" ,(err,resp) =>{
+                //check following existing user
                 if(err){
                     console.error("Checking follow user with a vaild ID that is already followed:");
                     console.error(err);
@@ -48,15 +50,58 @@ let followsTest = {
                     console.log(resp);
                 }
             });
-
+            Follows.followUser("60f490047843790" ,(err,resp) =>{
+                //check following invalid user id [numbers]
+                if(err){
+                    console.error("Checking follow user with an invaild id length :");
+                    console.error(err);
+                }
+                else{
+                    console.log("Checking follow user with an invalid id length :");
+                    console.log(resp);
+                }
+            });
+            Follows.followUser("*&^!@#$aTYa" ,(err,resp) =>{
+                //check following invalid user id [char]
+                if(err){
+                    console.error("Checking follow user with an invaild special char ID :");
+                    console.error(err);
+                }
+                else{
+                    console.log("Checking follow user with an invalid special char ID :");
+                    console.log(resp);
+                }
+            });
+////////////check follow functionality for blocked list///////////////////
+            Follows.followUser("9876543456789123456789098765432123456789098765432345678" ,(err,resp) =>{
+                //check following invalid user id [long]
+                if(err){
+                    console.error("Checking follow user with an invaild long ID :");
+                    console.error(err);
+                }
+                else{
+                    console.log("Checking follow user with an invalid long ID :");
+                    console.log(resp);
+                }
+            });
+            Follows.followUser("                        " ,(err,resp) =>{
+                //check follow functionality for an invalid ID [spaces]
+                if(err){
+                    console.error("Checking follow user with an invaled ID:");
+                    console.error(err);
+                }
+                else{
+                    console.log("Checking follow user with an invalid ID:");
+                    console.log(resp);
+                }
+            });
         }
 
     },
     testGetAllFollowedUsers : function(){
         if(typeof(Follows.getFollowedUsers)=="undefined"){
             console.error("Get all followed users exist is failed");
-        }
-        else{
+        } else{
             console.log("Get all followed user exist is passed")
             Follows.getFollowedUsers((err , resp) => {
                 if(err) console.error(err);
@@ -64,6 +109,10 @@ let followsTest = {
             });
         }
     }
+
 };
-//followsTest.testFollowUser();
+Follows.clearAppData();
+followsTest.testExist();
+followsTest.testFollowUser();
 followsTest.testGetAllFollowedUsers();
+Follows.clearAppData();
