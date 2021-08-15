ed6b6ad422cf2860b4f24d226581f57311eef734
nadiakhouri1 <nkhouri@madaincorp.com>
2021-08-15 20:51:05 +0300

    jasmine & follow functionality
    follow functionality
    added jasmine framework

---
+++ control/test/Spec/follows.spec.js
@@ -0,0 +1,10 @@
+describe('Follows functionality',function(){
+    const follows = new Follows();
+    it('Follow functionality should be defined',function(){
+        expect(follows).toBeDefined('Follows functionality not defined');
+    });
+    it('Follow users functionality should be defined',function(){
+        expect(follows).toBeDefined('Follow users functionality not defined');
+    });
+
+});
