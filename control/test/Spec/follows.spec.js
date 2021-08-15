f8081f6c6c6934fdb9f5b05690d8762cc4f2e222
nadiakhouri1 <nkhouri@madaincorp.com>
2021-08-16 01:53:17 +0300

    follows edits
    follows

--- control/test/Spec/follows.spec.js
+++ control/test/Spec/follows.spec.js
@@ -1,10 +1,35 @@
-describe('Follows functionality',function(){
-    const follows = new Follows();
-    it('Follow functionality should be defined',function(){
-        expect(follows).toBeDefined('Follows functionality not defined');
-    });
-    it('Follow users functionality should be defined',function(){
-        expect(follows).toBeDefined('Follow users functionality not defined');
-    });
+describe('Follow()', function () {
 
+    beforeAll(function(){
+
+        const followUser = Follows.followUser;
+        this.followUser = followUser;
+
+    });
+    beforeEach(function(){
+
+    });
+    
+    describe('FollowUsers()', function () {
+
+        it('Follow function should be defined', function () {
+            expect(this.followUser).toBeDefined('Follows functionality not defined');
+        });
+        it('Follow users function should be defined', function () {
+            expect(this.followUser).toBeDefined('Follow users functionality not defined');
+        });
+        it('Follow users should not accept follow empty ID', function () {
+            //wrong check error returned ****
+            // spyOn(Follows.followUser,fUserId)
+            //expect(function(){Follows.followUser("")}).toHaveBeenCalled(Error)
+        });
+        it('Follow users should follow a valid user ID', function () {
+            expect(function () { Follows.followUser("123456789012345678901234") }).not.toThrow();
+        });
+        it('Follow users should follow another valid user ID', function () {
+            expect(function () { Follows.followUser("60f49004784379051298ea37") }).not.toThrow();
+        });
+        
+
+    });
 });
