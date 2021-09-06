5213243b659aa50de4e070c165101d6828e5db00
nadiakhouri1 <nkhouri@madaincorp.com>
2021-09-07 00:17:07 +0300

    covered deletePost function

--- control/test/Spec/posts.spec.js
+++ control/test/Spec/posts.spec.js
@@ -1,6 +1,8 @@
 describe('Posts()', function () {
   /* posts list ids :
   current user fixed post for checing without deleting or updating 613506917d8532067fdd4095
+  current user post with special character 61354486ddca14064e977c9f
+  current user public post 6136685f7d8532067fdd4e2e
   unfollowed user private post id 612e9ef743556405fd295b8b for userid 6126c0e5fe743405301f11e8
   unfollowed user public post id 612fc03755ddae066e0df10d for userid 604f8dcfbd6aa82ebb15fda2
   followed user post id 612e9fc455ddae066e0de3a2 for userid 60f49004784379051298ea37
@@ -23,7 +25,7 @@ describe('Posts()', function () {
 
     it('Add post with empty post image and text', function (done) {
       Posts.addPost('', (err) => {
-        expect(err).toEqual('Post text cannot be empty');
+        expect(err).toEqual('Post cannot be null');
         done();
       });
     });
@@ -450,7 +452,6 @@ describe('Posts()', function () {
     it('updatePost for pluginInstance', function (done) {
       Posts.updatePost('613506917d8532067fdd4095', { pluginInstance: { "pluginInstanceId": "1627334094776-047554258642281366", "pluginInstanceTitle": "communityFeedPluginnn" }, postText: 'updatePost for fixed post', postImages: null }, (err, resp) => {
         expect(err).toEqual(null);
-        console.log(resp)
         expect(resp.data).toEqual(jasmine.objectContaining({
           pluginInstance: { "pluginInstanceId": "1627334094776-047554258642281355", "pluginInstanceTitle": "communityFeedPlugin" },
           postImages: [],
@@ -528,7 +529,7 @@ describe('Posts()', function () {
               "pluginInstanceId": "1627334094776-047554258642281355",
               "pluginInstanceTitle": "communityFeedPlugin"
             },
-            "isPublic": true,
+            "isPublic": false,
             "_buildfire": {
               "index": {
                 "array1": [
@@ -644,6 +645,7 @@ describe('Posts()', function () {
 
     it('addPublicPost with post text and empty image', function (done) {
       Posts.addPublicPost({ postText: 'post text test example' }, (err, resp) => {
+        console.log('public',resp);
         expect(resp.data).toEqual(jasmine.objectContaining({
           postText: 'post text test example',
           postImages: []
@@ -754,14 +756,36 @@ describe('Posts()', function () {
 
   describe('searchPosts()',function(){
     it('searchPosts should be defined',function(){
-      expect(Posts.searchPosts).toBeDefined();
+      expect(Posts.searchPosts).toBeDefined('searchPosts functionality not defined');
     });
 
-    xit('searchPosts for current user', function (done) {
-      Posts.searchPosts({text : postText.value }, (err, resp) => {
+    it('searchPosts for empty text', function (done) {
+      Posts.searchPosts({text : '' }, (err, resp) => {
+        expect(err).toEqual('Option text cannot be empty');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+
+    it('searchPosts for null text', function (done) {
+      Posts.searchPosts({text : null }, (err, resp) => {
+        expect(err).toEqual('Option text cannot be empty');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+
+    xit('searchPosts text for current user', function (done) {
+      Posts.searchPosts({text : 'updatePost for fixed post' }, (err, resp) => {
         expect(err).toEqual(null);
-        expect(resp).toEqual(jasmine.objectContaining({
-          "id": "612e58190dbbbd0670656df4",
+        let respSize = resp.length;
+        for (let step = 0; step < respSize; step++) {
+          expect(err[step].data).toEqual(jasmine.objectContaining({
+            "postText": "updated-post text sample",
+         }));
+        }
+                /*expect(resp).toEqual(jasmine.objectContaining({
+          "id": "613506917d8532067fdd4095",
           "userToken": "public",
           "data": {
             "createdOn": "2021-08-31T16:26:01.189Z",
@@ -796,83 +820,229 @@ describe('Posts()', function () {
               }
             }
           }
-        }));
+        }));*/
         done();
       });
     });
 
-    xit('getById for post id does not exist', function (done) {
-      Posts.getById('612e58199cbbbd0670656df4', (err, resp) => {
-        expect(err).toEqual("Couldn't find post with this ID.");
-        expect(resp).toEqual(undefined);
+    xit('searchPosts for a one keyword text', function (done) {
+      Posts.searchPosts({text : 'sample' }, (err, resp) => {
+        for(let step = 0 ; step<resp.length ;step++){
+        expect(resp[step].data.postText).toContain('sample');
+        } 
+        expect(err).toBeUndefined();
         done();
       });
     });
 
-    xit('getById with invalid post id', function (done) {
-      Posts.getById('56789876', (err, resp) => {
-        expect(err).toEqual("Couldn't find post with this ID.j");
-        expect(resp).toEqual(undefined);
+    xit('searchPosts private post for unfollowed user', function (done) {
+      Posts.searchPosts({text : 'Hello this is a post :D'}, (err, resp) => {
+        for(let step=0 ; step < resp.length ; step++){
+        expect(resp[step].data.id).not.toEqual('612e9ef743556405fd295b8b');
+        }
+        expect(err).toEqual(undefined);
         done();
       });
     });
 
-    xit('getById with empty post id', function (done) {
-      Posts.getById('', (err, resp) => {
-        expect(err).toEqual("error");
-        expect(resp).toEqual(undefined);
+    xit('searchPosts public post for unfollowed user', function (done) {
+      Posts.searchPosts({text : 'post text sample'}, (err, resp) => {
+        for(let step = 0 ; step <resp.length ; step++){
+        expect(resp[step].id).not.toEqual('612fc03755ddae066e0df10d');
+        }
+        expect(resp.data).toBeUndefined();
         done();
       });
     });
 
-    xit('getById without providing post id', function (done) {
-      Posts.getById((err, resp) => {
-        expect(err).toEqual('Post id does not existt');
-        expect(resp).toEqual(undefined);
+    xit('searchPosts for post does not exist', function (done) {
+      Posts.searchPosts({text : ')(*&^%$#@!ASDFG60f49004784379051298ea3?><":}{PO'}, (err, resp) => {
+        expect(err).toEqual('Sorry, no matching posts were found.');
+        expect(resp).toEqual(null);
         done();
       });
     });
 
-    xit('getById private post for unfollowed user', function (done) {
-      Posts.getById('612e9ef743556405fd295b8b', (err, resp) => {
-        expect(err).toEqual('error');
-        expect(resp).toEqual(undefined);
-        done();
-      });
-    });
-
-    xit('getById public post for unfollowed user', function (done) {
-      Posts.getById('612fc03755ddae066e0df10d', (err, resp) => {
+    xit('searchPosts for special characters', function (done) {
+      Posts.searchPosts({ text: '%'}, (err, resp) => {
         expect(err).toEqual(null);
-        expect(resp.data).toEqual(jasmine.objectContaining({ 
-            userId: '604f8dcfbd6aa82ebb15fda2', 
-            postText: 'post text sample', 
-            postImages: 'image test', 
-            pluginInstance: Object({ 
-              pluginInstanceId: '1627334094776-047554258642281355', 
-              pluginInstanceTitle: 'communityFeedPlugin' }), 
-              isPublic: true, 
-               }));
+        for(let step = 0 ; step < resp.length ; step++){
+        expect(resp[step].data.postText).toContain('%');
+        expect(resp[step].id).toEqual('61354486ddca14064e977c9f');
+        }
         done();
       });
     });
 
-    xit('getById by userId', function (done) {
-      Posts.getById('60f49004784379051298ea37', (err, resp) => {
-        expect(err).toEqual('jlfjljf');
-        expect(resp).toEqual(undefined);
+    xit('searchPosts for short text', function (done) {
+      Posts.searchPosts({ text: 'on'}, (err, resp) => {
+        expect(err).toEqual(null);
+        for(let step = 0 ; step < resp.length ; step++){
+        expect(resp[step].data.postText).toContain('on');
+        }
         done();
       });
     });
 
-    xit('getById by displayName', function (done) {
-      Posts.getById({ displayName: 'test name'}, (err, resp) => {
-        expect(err).toEqual('oijojo');
-        expect(resp).toEqual(undefined);
+    xit('searchPosts for capital text', function (done) {
+      Posts.searchPosts({ text: 'SAMPLE'}, (err, resp) => {
+        expect(err).toEqual(null);
+        for(let step = 0 ; step < resp.length ; step++){
+        expect(resp[step].data.postText.toLocaleLowerCase()).toContain('SAMPLE'.toLocaleLowerCase());
+        }
         done();
       });
     });
 
   });
+
+  /******************************************/
+  /********* getCurrentUserPosts ***********/
+  /****************************************/
+
+  describe('getCurrentUserPosts()',function(){
+    it('getCurrentUserPosts should be defined',function(){
+      expect(Posts.getCurrentUserPosts).toBeDefined('getCurrentUserPosts functionality not defined');
+    });
+
+    it('getCurrentUserPosts for current user', function (done) {
+      Posts.getCurrentUserPosts({ userId: authManager.currentUser._id}, (err, resp) => {
+        expect(err).toEqual(null);
+        for(let step= 1 ; step < resp.length ; step++){
+        expect(resp[step].data.userId).toEqual(authManager.currentUser._id);
+      };
+        done();
+      });
+    });
+
+    xit('getCurrentUserPosts for current user public posts',function(done){
+      Posts.getCurrentUserPosts({isPublic : true}, (err,resp) => {
+        expect(err).toEqual(null);
+        for(let step = 0 ; step < resp.length ; step++){
+          expect(resp[step].data.isPublic).toEqual(true);
+          expect(resp[step].data.userId).toEqual(authManager.currentUser._id);
+        }
+      });
+      done();
+    })
+
+    xit('getCurrentUserPosts for current user private posts',function(done){
+      Posts.getCurrentUserPosts({isPublic : false}, (err,resp) => {
+        expect(err).toEqual(null);
+        for(let step = 0 ; step < resp.length ; step++){
+          expect(resp[step].data.isPublic).toEqual(false);
+          expect(resp[step].data.userId).toEqual(authManager.currentUser._id);
+        }
+      });
+      done();
+    })
+//check sorting
+
+  });
+
+  /******************************************/
+  /************* deletePost ****************/
+  /****************************************/
+
+  describe('deletePost()',function(){
+    it('deletePost should be defined',function(){
+      expect(Posts.deletePost).toBeDefined('deletePost functionality not defined');
+    });
+
+    xit('deletePost private post for current user',function(done){
+      var firstPost = '1234';
+      Posts.getPosts({postText : 'post',userId : authManager.currentUser._id, isPublic:false} , (err,resp)=>{
+        firstPost = resp[1].id;
+      Posts.deletePost(firstPost , (err , resp)=>{
+        expect(err).toEqual('fff');
+        expect(resp).toEqual('deleted');      
+      });
+      done();
+      });
+      /*deleteSamplePost = 'new';
+      Posts.addPost({postText : 'post for deleting purposes',isPublic : false},(err,resp) =>{
+        deleteSamplePost = resp.id;
+        console.log('delete',deleteSamplePost);
+        Posts.deletePost((deleteSamplePost), (errr,r) => {
+        expect(errr).toEqual(null);
+        expect(r.status).toEqual('deleted');
+        Posts.getById(deleteSamplePost,(er,re) => {
+          expect(er).toEqual(null);
+          expect(re.id).toEqual(deleteSamplePost);
+        });
+      });
+    });*/      
+    });
+
+    xit('deletePost public post for current user',function(done){
+      var firstPost = '1234';
+      Posts.getPosts({userId : authManager.currentUser._id, isPublic:true} , (err,resp)=>{
+        firstPost = resp[1].id;
+      Posts.deletePost(firstPost , (err , resp)=>{
+        expect(err).toEqual('fff');
+        expect(resp).toEqual('deleted');      
+      });
+      done();
+      });
+    });
+
+    xit('deletePost post with image only for current user',function(done){
+      var firstPost = '1234';
+      Posts.getPosts({postText : 'image',userId : authManager.currentUser._id, isPublic:true} , (err,resp)=>{
+        firstPost = resp[1].id;
+      Posts.deletePost(firstPost , (err , resp)=>{
+        expect(err).toEqual(undefined);
+        expect(resp).toEqual('deleted');      
+      });
+      done();
+      });
+    });
+
+    xit('deletePost for unexistent post',function(done){
+      Posts.deletePost('123454321234543', (err,resp) => {
+        expect(err).toEqual('You can only delete your own posts');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+
+    xit('deletePost for empty post id',function(done){
+      Posts.deletePost('', (err,resp) => {
+        expect(err).toEqual('You can only delete your own posts');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+
+    xit('deletePost for null post id',function(done){
+      Posts.deletePost(null, (err,resp) => {
+        expect(err).toEqual('You can only delete your own posts');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+
+    xit('deletePost public post for another user',function(done){
+      Posts.deletePost('612fc03755ddae066e0df10d', (err,resp) => {
+        expect(err).toEqual('You can only delete your own posts');
+        expect(resp).toBeUndefined();
+        done();
+      });
+    });
+    
+    xit('deletePost private post for another user',function(done){
+      var firstPost = '1234';
+      Posts.getPosts({userId : '60f49004784379051298ea37', isPublic:false} , (err,resp)=>{
+        firstPost = resp[1].id;
+      Posts.deletePost(firstPost , (err , resp)=>{
+        expect(err).toEqual('You can only delete your own posts');
+        expect(resp).toEqual(undefined);      
+      });
+      done();
+      });
+    });
+//check sorting
+
+  })
 
 });
