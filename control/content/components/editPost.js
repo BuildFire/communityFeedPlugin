3200bf19110b9f63deffab8722e7db89955179d9
alikaakati <kaakatiali@gmail.com>
2021-10-28 17:20:32 +0300

    added form validation in updating a post

--- control/content/components/editPost.js
+++ control/content/components/editPost.js
@@ -10,26 +10,19 @@ const render = (id) =>{
 }
 
 const addPost = () =>{
-    let title = document.getElementById("title");
-    let description = document.getElementById("description");
-    title.classList.remove("error");
-    description.classList.remove("error");
+    clearAllErrors();
+        validateForm((err,r) =>{
+            if(err) return;
+            let title = document.getElementById("title");
+            let description = document.getElementById("description");
+            Posts.updatePublicPost(objid,{postTitle:title.value,postText:description.value},(err,r) =>{
+                if(err || !r) return console.error(err);
+                if(r){
+                    window.location.href = "../index.html"
+                }   
+            })
+        });
 
-    if(!title.value.replace(/\s/g, "")){
-    title.classList.add("error")
-    }
-    else if(!description.value.replace(/\s/g, "")){                
-    description.classList.add("error")
-    }
-    else{
-
-    Posts.updatePublicPost(objid,{postTitle:title.value,postText:description.value},(err,r) =>{
-    if(err || !r) return console.error(err);
-    if(r){
-    window.location.href = "../index.html"
-    }   
-    })
-    }
 
 }
 
